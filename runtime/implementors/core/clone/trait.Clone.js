2272b756cd6a1875074f7b3f65467f5356316af1
CensoredUsername <cens.username@gmail.com>
2016-08-27 01:53:26 +0200

    Rebuild docs

--- runtime/implementors/core/clone/trait.Clone.js
+++ runtime/implementors/core/clone/trait.Clone.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["dynasmrt"] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/clone/trait.Clone.html' title='core::clone::Clone'>Clone</a> for <a class='struct' href='dynasmrt/struct.Executor.html' title='dynasmrt::Executor'>Executor</a>",];
+implementors["dynasmrt"] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/clone/trait.Clone.html' title='core::clone::Clone'>Clone</a> for <a class='struct' href='dynasmrt/struct.AssemblyOffset.html' title='dynasmrt::AssemblyOffset'>AssemblyOffset</a>","impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/clone/trait.Clone.html' title='core::clone::Clone'>Clone</a> for <a class='struct' href='dynasmrt/struct.DynamicLabel.html' title='dynasmrt::DynamicLabel'>DynamicLabel</a>","impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/clone/trait.Clone.html' title='core::clone::Clone'>Clone</a> for <a class='struct' href='dynasmrt/struct.Executor.html' title='dynasmrt::Executor'>Executor</a>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
